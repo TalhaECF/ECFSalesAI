d6eba6a03f9ef13d02fe5254a700533f66d8370f
Talha Jaleel <talha.jaleel@ecfdata.com>
2024-12-24 19:12:36 +0500

    updated APIVIEWs

--- ai_app/urls.py
+++ ai_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('redirect/', OAuthRedirectView.as_view(), name='oauth_redirect'),
     path('discovery/', DiscoveryQuestionnaireView.as_view(), name='discovery_questionnaire'),
     path('wbs/', WBSDocumentView.as_view(), name='wbs_document'),
-    path('form_response/', InitialFormResponseView.as_view(), name='initial_form_with_transcript')
+    path('form_response/<str:project_id>/', InitialFormResponseView.as_view(), name='initial_form_with_transcript'),
 ]
--- ai_app/views.py
+++ ai_app/views.py
@@ -41,21 +41,15 @@ class InitialFormResponseView(APIView):
     """
     Fetch the Discovery Questionnaire file for a specific project ID.
     """
-    def post(self, request):
+    def get(self, request, project_id):
         try:
             site_id = config("SITE_ID")
             # Define the library path for Discovery Questionnaire files
             discovery_library_path = "Documents"
 
-            # Get project_id from the request body
-            project_id = request.data.get("project_id")
-            if not project_id:
-                return Response({"error": "Project ID is required."}, status=400)
-
             # Get access token
             access_token = get_access_token()
             # print(f"TOKEN: {access_token}")
-
             # Fetch the file
             # file_data = get_file_by_project_id(
             #     site_id=site_id,
@@ -68,6 +62,7 @@ class InitialFormResponseView(APIView):
             return Response("SUCCESS", status=200)
         except Exception as e:
             return Response({"error": str(e)}, status=500)
+
 
 
 class DiscoveryQuestionnaireView(APIView):
