d7e1c6294dc7c0dd2fa1818615fe3232dc24e45e
Talha Jaleel <talha.jaleel@ecfdata.com>
2025-01-30 16:50:45 +0500

    added prod base url in allowed hosts

--- ECFSalesAI/settings.py
+++ ECFSalesAI/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # https://d0f5-20-172-5-28.ngrok-free.app/redirect/
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecfsalesai-c7g9gtgmeceacggk.eastus2-01.azurewebsites.net',
-                 'ecfsalesai2-c4fsb9fjadb8gfb7.eastus2-01.azurewebsites.net']
+                 'ecfsalesai2-c4fsb9fjadb8gfb7.eastus2-01.azurewebsites.net', 'ecfsalesproduction-emd0ajgybzdyhndt.eastus2-01.azurewebsites.net']
 
 
 # Application definition
